c3da4edef59366116c2dab7a3035f7455dccecb2
IamtherealTuna <51160627+IamtherealTuna@users.noreply.github.com>
2025-05-03 19:59:12 -0500

    Create default.nix

---
+++ nix/default.nix
@@ -0,0 +1,51 @@
+{
+  stdenvNoCC,
+  lib,
+  sddm,
+  qtbase,
+  qtsvg,
+  qtquickcontrols2,
+  qtgraphicaleffects,
+  wrapQtAppsHook,
+  version ? "git",
+  themeConf ? ../theme.conf,
+}:
+stdenvNoCC.mkDerivation rec {
+  pname = "sddm-lain-wired-theme";
+  inherit version;
+
+  dontBuild = true;
+
+  src = lib.cleanSourceWith {
+    filter = name: type:
+      (builtins.match ".*(nix)" name) == null;
+    src = lib.cleanSourceWith {
+      filter = name: type: let
+        basename = builtins.baseNameOf name;
+      in
+        (builtins.match "(flake\.lock)|(props\.json)" basename) == null;
+      src = lib.cleanSource ../.;
+    };
+  };
+
+  propagatedUserEnvPkgs = [
+    sddm
+    qtbase
+    qtsvg
+    qtgraphicaleffects
+    qtquickcontrols2
+  ];
+
+  nativeBuildInputs = [
+    wrapQtAppsHook
+  ];
+
+  installPhase = ''
+    local installDir=$out/share/sddm/themes/${pname}
+    mkdir -p $installDir
+    cp -aR -t $installDir Main.qml Assets Components metadata.desktop theme.conf Backgrounds
+
+    # Applying theme
+    cat "${themeConf}" > "$installDir/theme.conf"
+  '';
+}
